c75e717b17d386aee4d02dcc952d028885dd23f7
BenC0 <suddensquirrel94@gmail.com>
2020-02-23 17:48:44 +0000

    Menu, Theme, Nav, demo content all styled up.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 let landingTheme = document.body.getAttribute('theme')
 if (landingTheme === null) {
-	document.body.setAttribute('theme', 2)
-	landingTheme = 1
+	document.body.setAttribute('theme', "day")
+	landingTheme = "day"
 }
+
+function changeThemeColor() {
+    let nextTheme = document.body.getAttribute('theme') === "day" ? "night" : "day"
+	document.body.setAttribute('theme', nextTheme)
+
+    var metaThemeColor = document.querySelector("meta[name=theme-color]");
+    metaThemeColor.setAttribute("content", nextTheme === "day" ? "#2a9d8f" : "#011627");
+}
+
+document.querySelector('nav .theme_swapper').addEventListener('click', changeThemeColor)
+
+document.querySelector('nav .menu').addEventListener('click', _ => {
+    let nextTheme = document.body.getAttribute('nav') === "active" ? "inactive" : "active"
+	document.body.setAttribute('nav', nextTheme)
+})
